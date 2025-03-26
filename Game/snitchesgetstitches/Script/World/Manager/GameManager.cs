3da23c7a3ab61e2122a2299a0f8b892bde22d695
Ashhuby <itsashhuby@gmail.com>
2025-03-26 21:16:32 +0000

    GameManager

--- Game/snitchesgetstitches/Script/World/Manager/GameManager.cs
+++ Game/snitchesgetstitches/Script/World/Manager/GameManager.cs
@@ -3,7 +3,8 @@ using System;
 
 public partial class GameManager : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] Label scoreLabel;
+	public int score = 0;
 	public override void _Ready()
 	{
 		GD.Print("Mananger Ready");
@@ -12,10 +13,12 @@ public partial class GameManager : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		scoreLabel.Text = "Score: " + score;
 	}
 
 	private void _on_successful_avoidance_area_area_entered(Area2D area)
 	{
 		GD.Print("Im hit");
+		score++;
 	}
 }
