bde974c1f107b51d5a933fcf511b86bc9d2caf39
Ashhuby <itsashhuby@gmail.com>
2025-04-02 09:08:10 +0100

    Hazard Sprites

--- Game/snitchesgetstitches/Script/Hazards/Objects/BaseHazard.cs
+++ Game/snitchesgetstitches/Script/Hazards/Objects/BaseHazard.cs
@@ -1,14 +1,54 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class BaseHazard : Node2D
 {
 	[Export] public float Speed = -9;
 	public int originSpawnerIndex = -1;
 	const float TargetFrameRate = 60f;
+	[Export] public Node2D[] TopMidHazardSprites;
+	[Export] public Node2D[] BotHazardSprites;
 	public override void _Ready()
 	{
 		AddToGroup("Hazards");
+		RandomNumberGenerator rng = new RandomNumberGenerator();
+		GD.Print(BotHazardSprites.Length);
+		GD.Print(TopMidHazardSprites.Length);
+
+		//Bot
+		if(originSpawnerIndex == 2)
+		{
+			int BotSprite = rng.RandiRange(0,BotHazardSprites.Length-1);
+			for(int i = 0; i < BotHazardSprites.Length;i++)
+			{
+				if(i == BotSprite)
+				{
+					BotHazardSprites[i].Visible = true;
+					GD.Print(BotHazardSprites[i].Name);				}
+				else
+				{
+					BotHazardSprites[i].Visible = false;
+				}
+			}
+
+		}
+		else //TopMid
+		{
+			int chosenSpriteIndex = rng.RandiRange(0,TopMidHazardSprites.Length-1);
+			for(int i = 0; i < TopMidHazardSprites.Length;i++)
+			{
+				if(i == chosenSpriteIndex)
+				{
+					TopMidHazardSprites[i].Visible = true;
+					GD.Print(TopMidHazardSprites[1].Name);
+				}
+				else
+				{
+					TopMidHazardSprites[i].Visible = false;
+				}
+			}
+		}
 	}
 
 	public override void _Process(double delta)
